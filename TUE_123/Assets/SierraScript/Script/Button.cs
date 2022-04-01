bd97ad5549c42f55e617e54db96985c6b6297975
sierra0330 <50917875+sierra0330@users.noreply.github.com>
2022-04-01 20:19:33 +0800

    commit_new
    記得要用 2019.4.22f1

---
+++ TUE_123/Assets/SierraScript/Script/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene01()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
